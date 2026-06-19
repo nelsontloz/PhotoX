d5ac684fcef65a085dce213a4f012b60f79dadc4
Jose Nelson Alcocer Amurrio <nelsontloz@gmail.com>
2026-06-19 18:50:49 +0100

    fix: use dynamic import of AppModule so testcontainers env vars are read correctly

--- apps/user-service/test/integration/health.controller.spec.ts
+++ apps/user-service/test/integration/health.controller.spec.ts
@@ -2,7 +2,6 @@ import { type INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import supertest from 'supertest'
 import type { Server } from 'node:http'
-import { AppModule } from '../../src/app.module'
 import { setupTestInfra, teardownTestInfra } from './test-setup'
 
 interface HealthResponse {
@@ -21,6 +20,8 @@ let httpServer: Server
 
 beforeAll(async () => {
   await setupTestInfra()
+
+  const { AppModule } = await import('../../src/app.module')
 
   const module = await Test.createTestingModule({
     imports: [AppModule],
